f8fc975fe7bb30f14c6cf5f26a4508e3736974ab
Dalibor Delipara <dalibor.d@vivifyideas.com>
2018-09-27 16:47:29 +0200

    initial commit

---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,66 @@
+<template>
+    <section>
+        <div class='container is-fluid'>
+                
+             <b-field label="Title">
+                <b-input 
+                    placeholder="Name of your task..."
+                    type="search"
+                    v-model='title'
+                >
+                </b-input>
+            </b-field>
+            <b-field label="Description">
+                <b-input 
+                    maxlength="200" 
+                    type="textarea"
+                    placeholder="What do you need to do exactly ..."
+                    v-model='description'
+                >
+                </b-input>
+            </b-field>     
+                <p class="control">
+                    <button 
+                    type='submit' 
+                    class="button is-primary"
+                    v-on:click="sendTodo"
+                    >
+                    Create
+                    </button>
+                </p>
+                <p>{{title}}</p>
+                <p>{{description}}</p>
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default{
+    name: 'CreateTodo',
+    data(){
+        return {
+            title: '',
+            description: '',
+        }
+    },
+    methods: {
+        sendTodo: function(){
+            alert('radi');
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+button{
+    display: block;
+    width: 80px;
+    font-size: 1.2em;
+    padding: 2px;
+    margin: 10px auto;
+}
+
+</style>
+
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,63 @@
+<template>
+    <section>
+        <div class='container is-fluid'>
+                
+             <b-field label="Email">
+                <b-input 
+                    type="is-success"
+                    v-model='email'
+                >
+                </b-input>
+            </b-field>
+            <b-field label="Password">
+                <b-input 
+                    type="password"
+                    v-model='password'
+                >
+                </b-input>
+            </b-field>     
+                <p class="control">
+                    <button 
+                    type='submit' 
+                    class="button is-primary"
+                    v-on:click="login"
+                    >
+                    Login
+                    </button>
+                </p>
+                <p>{{title}}</p>
+                <p>{{description}}</p>
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default{
+    name: 'LoginPage',
+    data(){
+        return {
+            email: '',
+            password: '',
+        }
+    },
+    methods: {
+        login: function(){
+            alert('radi login');
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+button{
+    display: block;
+    width: 80px;
+    font-size: 1.2em;
+    padding: 2px;
+    margin: 10px auto;
+}
+
+</style>
+
